7bc58fc085681b82b1517795ca2c03dce1033265
RoyCal <vito.elias9@gmail.com>
2026-04-30 11:54:07 -0300

    Subtotal & Total methods

--- src/main/java/com/vito/course/entities/OrderItem.java
+++ src/main/java/com/vito/course/entities/OrderItem.java
@@ -15,13 +15,13 @@ import jakarta.persistence.Table;
 public class OrderItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@EmbeddedId
 	private OrderItemPK id = new OrderItemPK();
-	
+
 	private Integer quantity;
 	private Double price;
-	
+
 	public OrderItem() {
 	}
 
@@ -31,20 +31,20 @@ public class OrderItem implements Serializable {
 		this.quantity = quantity;
 		this.price = price;
 	}
-	
+
 	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
-	
+
 	public void setOrder(Order order) {
 		id.setOrder(order);
 	}
-	
+
 	public Product getProduct() {
 		return id.getProduct();
 	}
-	
+
 	public void setProduct(Product product) {
 		id.setProduct(product);
 	}
@@ -70,6 +70,10 @@ public class OrderItem implements Serializable {
 		return Objects.hash(id);
 	}
 
+	public Double getSubTotal() {
+		return quantity*price;
+	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -81,5 +85,5 @@ public class OrderItem implements Serializable {
 		OrderItem other = (OrderItem) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }

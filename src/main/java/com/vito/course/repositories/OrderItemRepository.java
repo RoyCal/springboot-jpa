24e125445016ae57440653d569aeb2c497cad148
RoyCal <vito.elias9@gmail.com>
2026-04-30 11:30:50 -0300

    ProductOrderItem one-to-many association

--- src/main/java/com/vito/course/repositories/OrderItemRepository.java
+++ src/main/java/com/vito/course/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.vito.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vito.course.entities.OrderItem;
+import com.vito.course.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
